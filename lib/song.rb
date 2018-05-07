dc7112a13f514bdb480032cda70ecd9aa21a8cb5
Paul <pcherian10@gmail.com>
2018-05-07 16:26:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   end
 
   def self.find_by_name (name)
-    @@all.detect {|song_name| name if name == song_name}
+    @@all.detect {|song_name| song_name if name == song_name}
   end
 
 end
