8b52190de99b5c1f1dac28ecaefb7b4f840e346b
Paul <pcherian10@gmail.com>
2018-05-07 18:05:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,8 +43,8 @@ class Song
   def self.new_from_filename (file_name)
     song = self.new
     file_name = file_name.split(/[.,-]/) #splitting using regex, then first and second would artist and name respectively.
-    song.artist_name = file_name[0].strip.artist_name
-    song.name = file_name[1].strip.name
+    song.artist_name = file_name[0].strip
+    song.name = file_name[1].strip
     song
   end
 
